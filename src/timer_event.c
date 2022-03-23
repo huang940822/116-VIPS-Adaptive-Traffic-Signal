ace81f815edf7124706e16929441d06703f4bb97
xueyang0312 <jeak831130015@gmail.com>
2022-03-23 10:56:42 +0800

    remove duplicate 5F4C

--- src/timer_event.c
+++ src/timer_event.c
@@ -66,8 +66,6 @@ void timer_event_handler(__sigval_t value)
         WAIT_ACK_LOOP
         temp_ack_seq = tsc_5F48();  //查詢目前時制計劃內容
         WAIT_ACK_LOOP
-        temp_ack_seq = tsc_5F4C();  //查詢號控器目前時相及步階
-        WAIT_ACK_LOOP
         if (count == 0) {
             temp_ack_seq = tsc_0F42();  //查詢日期、時間
             WAIT_ACK_LOOP
