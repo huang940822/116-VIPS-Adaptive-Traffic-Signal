c799fdd9ef00bfa8707becfe3ab345ef28df916c
xueyang0312 <jeak831130015@gmail.com>
2022-03-23 11:14:20 +0800

    Remove config error return

--- application/EVSP/src/EVSP_config.c
+++ application/EVSP/src/EVSP_config.c
@@ -37,7 +37,6 @@ int EVSP_config_init()
     fp = fopen(EVSP_CONFIG_FILE, "r");
     if (fp == NULL) {
         log_file_write_fatal_error("error opening %s", EVSP_CONFIG_FILE);
-        return CONFIG_INVALID_EVSP_OPEN_FILE;
     } else {
         snprintf(log_content + strlen(log_content),
                  LOG_CONTENT_LEN - strlen(log_content),
