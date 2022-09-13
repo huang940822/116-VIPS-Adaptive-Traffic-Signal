c735d5fda0fca2cab94fce254fb81d0733288acf
qwe661234 <qwe661234@gmail.com>
2022-09-13 19:02:55 +0800

    Add start shell script to protect from executing repeatly.

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+tmp=`ps -aux | grep middleware | cut -d ' ' -f 2-6,25-40`     
+
+prev=0
+for i in $tmp
+do
+    if [[ "$i" == *"build/exec/middleware"* ]]
+    then
+        echo "$i is executing with pid $prev"
+        exit 
+    fi
+    prev=$i
+done
+
+echo "Build middleware"
+sudo ./build/exec/middleware
