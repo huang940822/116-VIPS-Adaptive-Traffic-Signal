c7c29b8e60f755bc399ba3ebf1076ede236865c0
a12345645 <a12345645kimo.com@gmail.com>
2022-11-22 11:24:45 +0800

    spat stuck at loop

--- application/SPaT/src/SPaT_utils.c
+++ application/SPaT/src/SPaT_utils.c
@@ -211,7 +211,7 @@ int spat_msg_update(SPAT **pp_spat)
         Red[phase] = second - signal_status.plan[phase].AllRed;
     }
     // other phase
-    if (signal_status.SubPhaseCount == 0)
+    if (signal_status.SubPhaseCount == 0 || phase >= signal_status.SubPhaseCount)
         return -1;
     for (int i = (phase + 1) % signal_status.SubPhaseCount, j = phase; 
          i != phase; i = (i + 1) % signal_status.SubPhaseCount, 
