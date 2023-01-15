b8893e1d8aa180d317cfc113c28dce12c4f5a7f4
xueyang0312 <jeak831130015@gmail.com>
2023-01-16 03:49:35 +0800

    clear PHASE_WEIGHT_ERR 當收到其他策略的封包

--- application/TSP/src/TSP.c
+++ application/TSP/src/TSP.c
@@ -411,6 +411,7 @@ int TSP_on_cloud_packet_rx(void *arg)
                             LOG_CONTENT_LEN - strlen(log_content),
                             "\nWarning: error opening ./config/config.txt ");
                     } else{
+                        clear_CLOUD_PACKET_CHANGE_STRATEGY_2_PHASE_WEIGHT_ERR();
                         fprintf(outfile,"RSU_ID \"%s\"\n",config.RSU_id);
                         fprintf(outfile,"RSU_LAT %f\n",config.RSU_lat);
                         fprintf(outfile,"RSU_LON %f\n",config.RSU_lon);
