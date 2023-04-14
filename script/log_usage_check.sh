d5cb649318dd195b53735c50f6821b7da8c39b75
a12345645 <a12345645kimo.com@gmail.com>
2023-04-14 10:57:18 +0800

    log_usage_check path and storage dev

--- script/log_usage_check.sh
+++ script/log_usage_check.sh
@@ -7,26 +7,25 @@ path=$(cd "$(dirname "$0")"; pwd)
 
 while true
 do
-	
-    	available_usage=$(df | grep '/dev/sdb2' | awk '{ print $4 }')
-	while [ $available_usage -lt $threshold ]
-	#while true
-	do
-    	cd $path/log
-		echo "delete oldest files"
-		# delete the oldest file
-    	ls -1t | tail -n -1 | xargs -d "\n" -I {} rm {}
-    	available_usage=$(df | grep '/dev/sdb2' | awk '{ print $4 }')
-		echo $available_usage
-		
-	done
-	if [[ -f "empty" ]]
-        then
-            echo "empty file exist"
-        else
-            echo "create file named empty"
-            touch empty
-        fi
-	sleep 1
-done
 
+        available_usage=$(df | grep '/dev/sda2' | awk '{ print $4 }')
+        while [ $available_usage -lt $threshold ]
+        #while true
+        do
+                cd $path/log
+                echo "delete oldest files"
+                # delete the oldest file
+                ls -1t | tail -n -1 | xargs -d "\n" -I {} rm {}
+                available_usage=$(df | grep '/dev/sda2' | awk '{ print $4 }')
+                echo $available_usage
+
+        done
+        if [[ -f "empty" ]]
+        then
+                echo "empty file exist"
+        else
+                echo "create file named empty"
+                touch empty
+        fi
+        sleep 1
+done
