190d6b9815c8eea15fd776bc310744c7bcdec56e
a12345645 <a12345645kimo.com@gmail.com>
2023-07-24 01:10:53 +0800

    SPaT spat_set_state

--- application/TIB/src/TIB_SPaT_utils.c
+++ application/TIB/src/TIB_SPaT_utils.c
@@ -54,6 +54,103 @@ int spat_msg_init(SPAT **pp_spat)
 #define LeftGreen 0b00001000
 #define StrightGreen 0b00010000
 #define RightGreen 0b00100000
+const uint8_t signal_mask_arr[] = {RroundHeadGreen, LeftGreen, StrightGreen, RightGreen};
+
+#define after_cur_step(_eventState, _signalType)                           \
+    do {                                                                   \
+        index = state->state_time_speed.count++;                           \
+        state->state_time_speed.tab[index].eventState = _eventState;       \
+        state->state_time_speed.tab[index].timing.startTime_option = TRUE; \
+        state->state_time_speed.tab[index].timing.startTime = offset;      \
+        printf("%d ", offset);                                             \
+        offset += signal_status->plan[i]._signalType;                      \
+        state->state_time_speed.tab[index].timing.minEndTime = offset;     \
+        printf("%d ,", offset);                                            \
+    } while (0)
+
+#define run_a_cycle()                                                   \
+    do {                                                                \
+        for (int k = (cur_subphase + 1) % signal_status->SubPhaseCount; \
+             k != i; k = (k + 1) % signal_status->SubPhaseCount) {      \
+            offset += signal_status->plan[k].Green;                     \
+            offset += signal_status->plan[k].Yellow;                    \
+            offset += signal_status->plan[k].AllRed;                    \
+        }                                                               \
+    } while (0)
+
+static void inline spat_set_state(MovementState *state, traffic_signal_status_t *signal_status, MovementPhaseState greenType, int i)
+{
+    state->state_time_speed.count = 0;
+    int cur_subphase = signal_status->SubPhaseID - 1;
+    if (i == cur_subphase) {
+        int index = state->state_time_speed.count++;
+        int offset = signal_status->StepSec;
+        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
+        state->state_time_speed.tab[index].timing.minEndTime = offset;
+        printf("%d ", offset);
+
+        if (signal_status->StepID == 1 || signal_status->StepID == 2 || signal_status->StepID == 3) {
+            state->state_time_speed.tab[index].eventState = greenType;
+            state->state_time_speed.tab[index].timing.startTime = -(signal_status->plan[i].Green - signal_status->StepSec);
+            printf("%d ,", -(signal_status->plan[i].Green - signal_status->StepSec));
+
+            after_cur_step(MovementPhaseState_protected_clearance, Yellow);
+            after_cur_step(MovementPhaseState_stop_And_Remain, AllRed);
+        } else if (signal_status->StepID == 4) {
+            state->state_time_speed.tab[index].eventState = MovementPhaseState_protected_clearance;
+            state->state_time_speed.tab[index].timing.startTime = -(signal_status->plan[i].Yellow - signal_status->StepSec);
+            printf("%d ,", -(signal_status->plan[i].Yellow - signal_status->StepSec));
+
+            after_cur_step(MovementPhaseState_stop_And_Remain, AllRed);
+            run_a_cycle();
+            after_cur_step(greenType, Green);
+
+        } else if (signal_status->StepID == 5) {
+            state->state_time_speed.tab[index].eventState = MovementPhaseState_stop_And_Remain;
+            state->state_time_speed.tab[index].timing.startTime = -(signal_status->plan[i].AllRed - signal_status->StepSec);
+            printf("%d ,", -(signal_status->plan[i].AllRed - signal_status->StepSec));
+
+            run_a_cycle();
+            after_cur_step(greenType, Green);
+            after_cur_step(MovementPhaseState_protected_clearance, Yellow);
+        }
+    } else {
+        int index = state->state_time_speed.count++;
+        state->state_time_speed.tab[index].eventState = MovementPhaseState_stop_And_Remain;
+        uint32_t offset = 0;
+        offset += signal_status->StepID >= 1 ? signal_status->plan[cur_subphase].Green : 0;
+        offset += signal_status->StepID >= 2 ? signal_status->plan[cur_subphase].PedGreenFlash : 0;
+        offset += signal_status->StepID >= 3 ? signal_status->plan[cur_subphase].PedRed : 0;
+        offset += signal_status->StepID >= 4 ? signal_status->plan[cur_subphase].Yellow : 0;
+        offset += signal_status->StepID >= 5 ? signal_status->plan[cur_subphase].AllRed : 0;
+
+        offset -= signal_status->StepSec;
+        offset += signal_status->plan[i].AllRed;
+        for (int k = (cur_subphase - 1 + signal_status->SubPhaseCount) % signal_status->SubPhaseCount;
+             k != i; k = (k - 1 + signal_status->SubPhaseCount) % signal_status->SubPhaseCount) {
+            offset += signal_status->plan[k].Green;
+            offset += signal_status->plan[k].Yellow;
+            offset += signal_status->plan[k].AllRed;
+        }
+        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
+        state->state_time_speed.tab[index].timing.startTime = -offset;
+        printf("*%d ", -offset);
+
+        offset = signal_status->StepSec;
+        offset += signal_status->StepID <= 1 ? signal_status->plan[i].PedGreenFlash : 0;
+        offset += signal_status->StepID <= 2 ? signal_status->plan[i].PedRed : 0;
+        offset += signal_status->StepID <= 3 ? signal_status->plan[i].Yellow : 0;
+        offset += signal_status->StepID <= 4 ? signal_status->plan[i].AllRed : 0;
+
+        run_a_cycle();
+        state->state_time_speed.tab[index].timing.minEndTime = offset;
+        printf("%d ,", offset);
+
+        after_cur_step(greenType, Green);
+        after_cur_step(MovementPhaseState_protected_clearance, Yellow);
+    }
+    printf("\n");
+}
 
 int spat_msg_update(SPAT *pp_spat)
 {
@@ -61,12 +158,12 @@ int spat_msg_update(SPAT *pp_spat)
     get_traffic_signal_status(&signal_status);
     if (signal_status.SubPhaseID == 0 || signal_status.StepID == 0)
         return -1;
+
     printf("signal_status.StepSec %d\n", signal_status.StepSec);
     printf("signal_status.StepID %d\n", signal_status.StepID);
     printf("signal_status.SubPhaseID %d\n", signal_status.SubPhaseID);
     printf("signal_status.SubPhaseCount %d\n", signal_status.SubPhaseCount);
-    printf("signal_status.plan[0].PedGreenFlash %d\n", signal_status.plan[0].PedGreenFlash);
-    printf("signal_status.plan[1].PedRed %d\n", signal_status.plan[1].PedRed);
+
     IntersectionState *int_state = pp_spat->intersections.tab;
     int_state->revision = (int_state->revision + 1) & 0b1111111;
 
@@ -86,130 +183,17 @@ int spat_msg_update(SPAT *pp_spat)
     int cur_subphase = signal_status.SubPhaseID - 1;
     for (int i = 0; i < signal_status.SubPhaseCount; i++) {
         for (int j = 0; j < signal_status.SignalCount; j++) {
-            if (signal_status.phaseorder_plan[i][j].SignalStatus & RroundHeadGreen && signalGroupID <= 255) {
-                MovementState *state = &statesList->tab[statesList->count++];
-                state->state_time_speed.count = 0;
-                state->signalGroup = signalGroupID;
-
-                if (i == cur_subphase) {
-                    int index = state->state_time_speed.count++;
-                    int offset = signal_status.StepSec;
-                    state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                    state->state_time_speed.tab[index].timing.minEndTime = offset;
-                    // printf("%d ", offset);
-
-                    if (signal_status.StepID == 1 || signal_status.StepID == 2 || signal_status.StepID == 3) {
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_permissive_Movement_Allowed;
-                        state->state_time_speed.tab[index].timing.startTime = -(signal_status.plan[i].Green - signal_status.StepSec);
-                        // printf("%d ", -(signal_status.plan[i].Green - signal_status.StepSec));
-
-                        index = state->state_time_speed.count++;
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_protected_clearance;
-                        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                        state->state_time_speed.tab[index].timing.startTime = offset;
-                        // printf("%d ", offset);
-                        offset += signal_status.plan[i].Yellow;
-                        state->state_time_speed.tab[index].timing.minEndTime = offset;
-                        // printf("%d ", offset);
-
-                        index = state->state_time_speed.count++;
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_stop_And_Remain;
-                        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                        state->state_time_speed.tab[index].timing.startTime = offset;
-                        // printf("%d ", offset);
-                        offset += signal_status.plan[i].AllRed;
-                        state->state_time_speed.tab[index].timing.minEndTime = offset;
-                        // printf("%d ", offset);
-
-                    } else if (signal_status.StepID == 4) {
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_protected_clearance;
-                        state->state_time_speed.tab[index].timing.startTime = -(signal_status.plan[i].Yellow - signal_status.StepSec);
-
-                        index = state->state_time_speed.count++;
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_stop_And_Remain;
-                        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                        state->state_time_speed.tab[index].timing.startTime = offset;
-                        offset += signal_status.plan[i].Yellow;
-                        state->state_time_speed.tab[index].timing.minEndTime = offset;
-
-                        index = state->state_time_speed.count++;
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_permissive_Movement_Allowed;
-                        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                        state->state_time_speed.tab[index].timing.startTime = offset;
-                        offset += signal_status.plan[i].AllRed;
-                        state->state_time_speed.tab[index].timing.minEndTime = offset;
-                    } else if (signal_status.StepID == 5) {
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_stop_And_Remain;
-                        state->state_time_speed.tab[index].timing.startTime = -(signal_status.plan[i].AllRed - signal_status.StepSec);
-
-                        index = state->state_time_speed.count++;
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_permissive_Movement_Allowed;
-                        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                        state->state_time_speed.tab[index].timing.startTime = offset;
-                        offset += signal_status.plan[i].Yellow;
-                        state->state_time_speed.tab[index].timing.minEndTime = offset;
-
-                        index = state->state_time_speed.count++;
-                        state->state_time_speed.tab[index].eventState = MovementPhaseState_protected_clearance;
-                        state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                        state->state_time_speed.tab[index].timing.startTime = offset;
-                        offset += signal_status.plan[i].AllRed;
-                        state->state_time_speed.tab[index].timing.minEndTime = offset;
+            for (int k = 0; k < sizeof(signal_mask_arr); k++) {
+                if (signal_status.phaseorder_plan[i][j].SignalStatus & signal_mask_arr[i] && signalGroupID <= 255) {
+                    MovementState *state = &statesList->tab[statesList->count++];
+                    state->signalGroup = signalGroupID++;
+                    if (k == 0) {
+                        printf("222-----\n");
+                        spat_set_state(state, &signal_status, MovementPhaseState_protected_Movement_Allowed, i);
+                    } else {
+                        printf("333-----\n");
+                        spat_set_state(state, &signal_status, MovementPhaseState_protected_Movement_Allowed, i);
                     }
-                } else {
-                    int index = state->state_time_speed.count++;
-                    state->state_time_speed.tab[index].eventState = MovementPhaseState_stop_And_Remain;
-                    uint32_t offset = 0;
-                    offset += signal_status.StepID >= 1 ? signal_status.plan[cur_subphase].Green : 0;
-                    offset += signal_status.StepID >= 2 ? signal_status.plan[cur_subphase].PedGreenFlash : 0;
-                    offset += signal_status.StepID >= 3 ? signal_status.plan[cur_subphase].PedRed : 0;
-                    offset += signal_status.StepID >= 4 ? signal_status.plan[cur_subphase].Yellow : 0;
-                    offset += signal_status.StepID >= 5 ? signal_status.plan[cur_subphase].AllRed : 0;
-
-                    offset -= signal_status.StepSec;
-                    offset += signal_status.plan[i].AllRed;
-                    for (int k = (signal_status.SubPhaseID - 2 + signal_status.SubPhaseCount) % signal_status.SubPhaseCount; k != i;
-                         k = (k - 1 + signal_status.SubPhaseCount) % signal_status.SubPhaseCount) {
-                        offset += signal_status.plan[k].Green;
-                        offset += signal_status.plan[k].Yellow;
-                        offset += signal_status.plan[k].AllRed;
-                    }
-                    state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                    state->state_time_speed.tab[index].timing.startTime = -offset;
-                    // printf("*%d ", -offset);
-
-                    offset = signal_status.StepSec;
-                    offset += signal_status.StepID <= 1 ? signal_status.plan[i].PedGreenFlash : 0;
-                    offset += signal_status.StepID <= 2 ? signal_status.plan[i].PedRed : 0;
-                    offset += signal_status.StepID <= 3 ? signal_status.plan[i].Yellow : 0;
-                    offset += signal_status.StepID <= 4 ? signal_status.plan[i].AllRed : 0;
-
-                    for (int k = (cur_subphase + 1) % signal_status.SubPhaseCount;
-                         k != i; k = (k + 1) % signal_status.SubPhaseCount) {
-                        offset += signal_status.plan[k].Green;
-                        offset += signal_status.plan[k].Yellow;
-                        offset += signal_status.plan[k].AllRed;
-                    }
-                    state->state_time_speed.tab[index].timing.minEndTime = offset;
-                    // printf("-%d ", offset);
-
-                    index = state->state_time_speed.count++;
-                    state->state_time_speed.tab[index].eventState = MovementPhaseState_permissive_Movement_Allowed;
-                    state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                    state->state_time_speed.tab[index].timing.startTime = offset;
-                    // printf("*%d ", offset);
-                    offset += signal_status.plan[i].Green;
-                    state->state_time_speed.tab[index].timing.minEndTime = offset;
-                    // printf("-%d ", offset);
-
-                    index = state->state_time_speed.count++;
-                    state->state_time_speed.tab[index].eventState = MovementPhaseState_protected_clearance;
-                    state->state_time_speed.tab[index].timing.startTime_option = TRUE;
-                    state->state_time_speed.tab[index].timing.startTime = offset;
-                    // printf("*%d ", offset);
-                    offset += signal_status.plan[i].Yellow;
-                    state->state_time_speed.tab[index].timing.minEndTime = offset;
-                    // printf("-%d ", offset);
                 }
             }
         }
