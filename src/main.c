c6e8822d1d07f2a3e4d337a12b695a6d67d4a895
a12345645 <a12345645kimo.com@gmail.com>
2023-10-06 06:59:07 +0800

    version update

--- src/main.c
+++ src/main.c
@@ -63,7 +63,7 @@ int main()
     /* log init */
     log_file_init();  //一個timer被created
 
-    log_file_write("version : v2.4.5");
+    log_file_write("version : v2.4.6");
 
     /* read config file*/
     ret = config_init();
