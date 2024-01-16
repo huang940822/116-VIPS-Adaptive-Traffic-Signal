6f4ec2554d5a05473b60725424eacf86adae240d
crlin <circlelin7@gmail.com>
2024-01-16 12:00:24 +0800

    version update

--- src/main.c
+++ src/main.c
@@ -101,7 +101,7 @@ int main()
     /* log init */
     log_file_init();  // 一個timer被created
 
-    log_file_write("version : v2.4.12");
+    log_file_write("version : v2.5.1");
 
     /* read config file*/
     ret = config_init();
