275dba9bde9557e8a911a7224b3a811bdc11c5f0
Kurumi2650 <kurumi2650@gmail.com>
2025-05-12 16:28:00 +0800

    recover the code that should not be modified

--- src/main.c
+++ src/main.c
@@ -181,11 +181,11 @@ int main()
     /* application service registration */ //目前先做交通號誌協定(TSP)和緊急交通工具號誌協定(EVSP)
     app_obj_t *app_arr[] = {
         // &MMP,
-        //&EVSP,
-        //&TSP,
+        &EVSP,
+        &TSP,
         // &CPS,
-        //&TIB,
-        //&SPM,
+        &TIB,
+        &SPM,
         &WA,
     };
 
