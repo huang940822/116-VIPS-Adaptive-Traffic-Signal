58a1efacea9e7462c1776c5b094bf23662420c0f
Kurumi2650 <kurumi2650@gmail.com>
2025-05-12 16:31:41 +0800

    recover the comment that should not be deleted

--- src/util.c
+++ src/util.c
@@ -30,7 +30,7 @@ double distance(double lat1, double lon1, double lat2, double lon2) {
     double a = pow(sin(dLat / 2), 2) +
                cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * pow(sin(dLon / 2), 2);
     double c = 2 * asin(sqrt(a));
-    double r = 6371.0;
+    double r = 6371.0;  // 6371 Radius of earth in kilometers. Use 3956 for miles
     return c * r * 1000;  
 }
 double bearing(double lat, double lon, double lat2, double lon2)
