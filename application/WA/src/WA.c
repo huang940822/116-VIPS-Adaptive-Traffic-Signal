6b8d3f7a03ee7b27e0e6d622a877623a6ffd23af
Kurumi2650 <kurumi2650@gmail.com>
2025-05-13 12:01:15 +0800

    Move the registration to the right place, add the general comment and log message

--- application/WA/src/WA.c
+++ application/WA/src/WA.c
@@ -34,6 +34,22 @@ wa_vehicle_t Leading_Vehicles[4]; // Leading Vehicles for four directions NSWE
 timer_t WA_Agent_timer_id;
 pthread_mutex_t mutex_LV = PTHREAD_MUTEX_INITIALIZER;
 
+app_obj_t WA = {
+    .name = "WA",
+    .id = WA_ID,
+    .priority = 3,
+    .on_OBU_packet_rx = NULL,
+    .on_OBU_packet_tx = NULL,
+    .on_RSU_packet_rx = NULL,
+    .on_RSU_packet_tx = NULL,
+    .on_cloud_packet_rx = NULL,
+    .on_cloud_packet_tx = NULL,
+    .on_camera_packet_rx = &WA_on_camera_packet_rx,
+    .on_traffic_signal_command_tx = NULL,
+    .on_registration = &WA_on_registration,
+    .next = NULL,
+};
+
 void initializationLeadingVehicles(wa_vehicle_t vehicles[], int size){
     for (int i = 0; i < size; i++){
         vehicles[i].speed = NO_VEHICLE;
@@ -51,26 +67,9 @@ void initializationLeadingVehicles(wa_vehicle_t vehicles[], int size){
     }
     LOG_MSG_TRACE(log_content);
 }
-app_obj_t WA = {
-    .name = "WA",
-    .id = WA_ID,
-    .priority = 3,
-    .on_OBU_packet_rx = NULL,
-    .on_OBU_packet_tx = NULL,
-    .on_RSU_packet_rx = NULL,
-    .on_RSU_packet_tx = NULL,
-    .on_cloud_packet_rx = NULL,
-    .on_cloud_packet_tx = NULL,
-    .on_camera_packet_rx = &WA_on_camera_packet_rx,
-    .on_traffic_signal_command_tx = NULL,
-    .on_registration = &WA_on_registration,
-    .next = NULL,
-};
 
 /* retrieve the speed and calculate the distance between leading vehicles and intersection */
 int WA_on_camera_packet_rx(void *arg){ 
-    char log_content[LOG_CONTENT_LEN + 1];
-    memset(log_content, 0, sizeof(log_content));
 
     LOG_MSG_TRACE("WA_on_camera_packet_rx called.");
 
@@ -83,6 +82,7 @@ int WA_on_camera_packet_rx(void *arg){
     leading_vehicle.speed = NO_VEHICLE;
     leading_vehicle.distance = NO_VEHICLE;
 
+    /* read the information that wa need from obstaclelist */
     for (int i = 0; i < obstaclelist->count; i++) {
         double lat = obstaclelist->tab[i].lat;
         double lon = obstaclelist->tab[i].Long;
@@ -101,7 +101,7 @@ int WA_on_camera_packet_rx(void *arg){
         double current_distance = distance(lat, lon, intersection_center_lat, intersection_center_lon);
         LOG_MSG_INFO("Incoming vehicle %d: lat=%.6f, lon=%.6f, speed=%.2f, distance=%.2f",
                        i, lat, lon, speed, current_distance);
-
+        /* find leading_vehicles */
         if (leading_vehicle.distance == NO_VEHICLE || current_distance < leading_vehicle.distance) {
             leading_vehicle.speed = speed * 0.2778; // km/hr to m/s
             leading_vehicle.distance = current_distance;
@@ -112,7 +112,7 @@ int WA_on_camera_packet_rx(void *arg){
     Leading_Vehicles[direct] = leading_vehicle;
     pthread_mutex_unlock(&mutex_LV);
 
-    LOG_MSG_INFO("Direction %d updated: Speed=%.2f, Distance=%.2f",
+    LOG_MSG_INFO("Updating leading vehicle in direction %d : Speed=%.2f, Distance=%.2f",
              direct, leading_vehicle.speed, leading_vehicle.distance);
 }
 
